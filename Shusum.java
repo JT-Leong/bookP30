72e7da54f385f71c167c9413679dffa4493de118
JT Leong <1433408558@qq.com>
2019-09-16 22:28:37 +0800

    book T1

---
+++ Shusum.java
@@ -0,0 +1,16 @@
+public class Shusum{
+	public static void main(String[] args){
+		sum();
+	}
+	public static void sum() {
+		int sum=0;
+		sum=34+30;
+		System.out.println("34+30="+sum);
+	}
+}
+
+/*public class Shusum{
+	public static void main(String[] args){
+		System.out.println("34+30="+(34+30));
+	}
+}*/
