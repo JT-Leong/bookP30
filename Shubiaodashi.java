e0bd4991c6ab14383699aa5977fe1a1f46b8e42e
JT <54793164+JT-Leong@users.noreply.github.com>
2019-09-16 22:52:41 +0800

    Add files via upload

---
+++ Shubiaodashi.java
@@ -0,0 +1,81 @@
+public class Shubiaodashi{
+	public static void main(String[] args){
+		result1();
+		result2();
+		result3();
+		result4();
+		result5();
+		result6();
+		result7();
+		result8();
+		result9();
+		result10();
+		result11();
+	}
+	public static void result1(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		res=n1/n4;
+		System.out.println(+res);
+	}
+	public static void result2(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		d=n1/n4;
+		System.out.println(+d);
+	}
+	public static void result3(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		res=n3/n4;
+		System.out.println(+res);
+	}
+	public static void result4(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		d=n3/n4;
+		System.out.println(+d);
+	}	
+	public static void result5(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		d=v1/n4;
+		System.out.println(+d);
+	}	
+	public static void result6(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		d=v1/v2;
+		System.out.println(+d);
+	}	
+	public static void result7(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		res=n1/n2;
+		System.out.println(+res);
+	}	
+	public static void result8(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		d=n1/n4;
+		System.out.println(+d);
+	}	
+	public static void result9(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		d=(double)n1/n2;
+		System.out.printf("%.2f",d);
+	}
+	public static void result10(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		d=n1/(double)n2;
+		System.out.printf("%.2f",d);
+	}	
+	public static void result11(){
+		int res,n1=12,n2=24,n3=16,n4=3;
+		double d,v1=10.0,v2=3.1416;
+		d=(double)n1/n2;
+		System.out.printf("%.2f",d);
+	}		
+}
---
+++ Shucount.java
@@ -0,0 +1,12 @@
+public class Shucount{
+	public static void main(String[] args){
+		count();
+	}
+	public static void count(){
+		int count=10;
+		count*=count+2;
+		count/=100;
+		count++;
+		System.out.println("count="+count);
+	}
+}
